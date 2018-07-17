946011058913dee306cbd7ec82eda40cf6173a0f
Jake Callaghan <jake.callaghan@isam.com>
2018-07-17 13:42:14 +0100

    added basic newsapi calls

--- news/news.py
+++ news/news.py
@@ -1,9 +1,20 @@
+import const
 from newsapi import NewsApiClient
+
 import sys
 sys.path.insert(0,"../")
 import config
 
 API = NewsApiClient(api_key=config.NEWS_API_KEY)
 
-def get_sources():
-	return API.get_sources()
+# returns live top/breaking headlines from the live sources only
+def get_headline_news(keyphrase, sources, country, category):
+	return API.get_top_headlines(q=keyphrase, sources=sources, language=config.DEFAULT_LANGUAGE, country=country, category=category)
+
+# returns the subset of sources that live headline news is coming from
+def get_headline_sources(category, country):
+	return API.get_sources(category=category, language=config.DEFAULT_LANGUAGE, country=country)
+
+# large search of millions of articles across ~5K sources
+def get_historic_news(keyprhase, sources, domains, fromDateStr, toDateStr, ordering):
+	return API.get_everything(q=keyphrase, sources=sources, domains=domains, from_param=fromDateStr, to_param=toDateStr, language=config.DEFAULT_LANGUAGE, sort_by=ordering, country=country)
--- news/test_news.py
+++ news/test_news.py
@@ -1,5 +1,11 @@
 import news
+import const
 
-def test_get_sources():
-	srcs = news.get_sources()
-	assert(srcs['status'] == 'ok')
+def test_const():
+	assert(len(const.CATEGORIES) > 0)
+	assert(len(const.COUNTRIES) > 0)
+	assert(len(const.LANGUAGES) > 0)
+	assert(len(const.LIVE_SOURCES) > 0)
+	assert(len(const.ORDERINGS) > 0)
+
+	
